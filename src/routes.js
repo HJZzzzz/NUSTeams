c33a3340333e483c453c19bb8f0f80c876771f53
sherrysheng0208 <35883711+sherrysheng0208@users.noreply.github.com>
2020-10-31 01:16:44 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -40,6 +40,8 @@ const Users = React.lazy(() => import('./views/users/Users'));
 const User = React.lazy(() => import('./views/users/User'));
 const ProjectDetails = React.lazy(() => import('./views/project/project-details/ProjectDetails'));
 // const Landing = React.lazy(() => import('./views/landing/Landing'));
+const ViewAllProjects = React.lazy(() => import("./views/view-all-projects/ViewAllProjects"));
+const ViewUsers = React.lazy(() => import("./views/view-users/ViewUsers"));
 
 const routes = [
   // { path: '/', exact: true, name: 'Home', component: Landing },
@@ -83,7 +85,9 @@ const routes = [
   { path: '/widgets', name: 'Widgets', component: Widgets },
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User },
-  { path: '/project/:id', exact: true, name: 'Project Details',component: ProjectDetails}
+  { path: '/project/:id', exact: true, name: 'Project Details',component: ProjectDetails},
+  { path: "/view-all-projects", exact: true, name: "View All Projects", component: ViewAllProjects },
+  { path: "/view-users", exact: true, name: "View Users", component: ViewUsers }
 ];
 
 export default routes;
