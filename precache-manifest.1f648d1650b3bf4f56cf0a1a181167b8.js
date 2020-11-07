1bec83e41c2e48e0525e31b566516bf73674bbeb
bryan <bryankoh2196@gmail.com>
2020-11-07 17:52:41 +0800

    Updates

---
+++ precache-manifest.1f648d1650b3bf4f56cf0a1a181167b8.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb3f809858e2bdebeb5c3061f602a56",
+    "url": "/NUSTeams/index.html"
+  },
+  {
+    "revision": "b221d65f8e178c614355",
+    "url": "/NUSTeams/static/css/0.e46473d8.chunk.css"
+  },
+  {
+    "revision": "cc7cac562716464d303d",
+    "url": "/NUSTeams/static/css/6.5614718e.chunk.css"
+  },
+  {
+    "revision": "e0fea8eb18ba0e961c34",
+    "url": "/NUSTeams/static/css/main.61acee8a.chunk.css"
+  },
+  {
+    "revision": "b221d65f8e178c614355",
+    "url": "/NUSTeams/static/js/0.7f2a6145.chunk.js"
+  },
+  {
+    "revision": "50657dd44cde48d36e24db270ce065f0",
+    "url": "/NUSTeams/static/js/0.7f2a6145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f48c08b4335b7891e76",
+    "url": "/NUSTeams/static/js/10.1a9e4e00.chunk.js"
+  },
+  {
+    "revision": "1e6c62abb71d0e431571",
+    "url": "/NUSTeams/static/js/11.d8b572aa.chunk.js"
+  },
+  {
+    "revision": "a6f6eceeea1945fb1bf8",
+    "url": "/NUSTeams/static/js/12.d3cb2b51.chunk.js"
+  },
+  {
+    "revision": "175e2828721cb1adb8b3",
+    "url": "/NUSTeams/static/js/13.8ea13592.chunk.js"
+  },
+  {
+    "revision": "56a9120ae5ae4d54c8e9",
+    "url": "/NUSTeams/static/js/14.2b372382.chunk.js"
+  },
+  {
+    "revision": "58eab8881bbc7d4a4d06",
+    "url": "/NUSTeams/static/js/15.79b3a159.chunk.js"
+  },
+  {
+    "revision": "ac1ac2486b2771b89679",
+    "url": "/NUSTeams/static/js/16.81e1af52.chunk.js"
+  },
+  {
+    "revision": "1bdd483e4baafd8557c1",
+    "url": "/NUSTeams/static/js/17.1b2594b1.chunk.js"
+  },
+  {
+    "revision": "5b4547be2fa0d3f5e223",
+    "url": "/NUSTeams/static/js/18.7386f1be.chunk.js"
+  },
+  {
+    "revision": "503c7dc34ed7522b83dc",
+    "url": "/NUSTeams/static/js/19.32ff01e7.chunk.js"
+  },
+  {
+    "revision": "8f081b3582992cb59d06",
+    "url": "/NUSTeams/static/js/20.e73c1575.chunk.js"
+  },
+  {
+    "revision": "472049cf4f58ff443cf9",
+    "url": "/NUSTeams/static/js/21.03747cad.chunk.js"
+  },
+  {
+    "revision": "5da24581aa00787ed509",
+    "url": "/NUSTeams/static/js/22.303b588f.chunk.js"
+  },
+  {
+    "revision": "27aa33f15adb612771d2",
+    "url": "/NUSTeams/static/js/23.375ecc88.chunk.js"
+  },
+  {
+    "revision": "08329408f2e8d5837f14",
+    "url": "/NUSTeams/static/js/3.53b5d029.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NUSTeams/static/js/3.53b5d029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c17ac656c86856a17a",
+    "url": "/NUSTeams/static/js/4.e42f4f37.chunk.js"
+  },
+  {
+    "revision": "558973fe9e5ef923b401",
+    "url": "/NUSTeams/static/js/5.d9e20c2c.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/NUSTeams/static/js/5.d9e20c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7cac562716464d303d",
+    "url": "/NUSTeams/static/js/6.af67a8f6.chunk.js"
+  },
+  {
+    "revision": "d8d8e1a461bc247fbd37ad846147df80",
+    "url": "/NUSTeams/static/js/6.af67a8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f529558c9a2cec2e7171",
+    "url": "/NUSTeams/static/js/7.ad209b15.chunk.js"
+  },
+  {
+    "revision": "37e100b994ca06d8eceb",
+    "url": "/NUSTeams/static/js/8.f11c588c.chunk.js"
+  },
+  {
+    "revision": "970cb3c78f182fffe27c",
+    "url": "/NUSTeams/static/js/9.1efb9319.chunk.js"
+  },
+  {
+    "revision": "e0fea8eb18ba0e961c34",
+    "url": "/NUSTeams/static/js/main.3f4fa8f5.chunk.js"
+  },
+  {
+    "revision": "250a800f7d8387f9d724",
+    "url": "/NUSTeams/static/js/runtime-main.e29e9ac9.js"
+  },
+  {
+    "revision": "99f531a714632f691021d49a62cc8382",
+    "url": "/NUSTeams/static/media/nus-home.99f531a7.png"
+  }
+]);
