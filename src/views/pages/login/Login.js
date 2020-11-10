851d1ad6482c02a418652bf47fe4ab550cddbd77
bryan <bryankoh2196@gmail.com>
2020-11-10 18:05:58 +0800

    read me

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -61,50 +61,50 @@ const Login = () => {
   }
 
   return (
-    <div className="c-app c-default-layout flex-row align-items-center" style={ homeImage }>
-        <CContainer >
-          <CRow className="justify-content-center">
-            <CCol md="7">
-              <CCardGroup>
-                <CCard className="p-4" style={{backgroundColor: 'white', opacity: 0.9 }}>
-                  <CCardBody className="text-center">
-                    <CForm>
-                      <span className="display-4">Welcome to NUSTeams</span>
-                      <p>Centralized collaboration platform for NUS students</p>
-                      <CInputGroup className="mb-3">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon name="cil-user" />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput color="black" type="text" placeholder="Username" autoComplete="username" onChange={handleEmailChange} />
-                      </CInputGroup>
-                      <CInputGroup className="mb-4">
-                        <CInputGroupPrepend>
-                          <CInputGroupText>
-                            <CIcon name="cil-lock-locked" />
-                          </CInputGroupText>
-                        </CInputGroupPrepend>
-                        <CInput type="password" placeholder="Password" autoComplete="current-password" onChange={handlePasswordChange} />
-                      </CInputGroup>
-                      <CRow>
-                        <CCol xs="6" className="text-left">
-                          <CButton color="link" className="px-0">Forgot password?</CButton>
-                        </CCol>
-                        <CCol xs="6" className="text-right">
-                          <CButton color="primary" className="px-4" onClick={doLogin}>Login</CButton>
-                        </CCol>
-                      </CRow>
-                    </CForm>
-                    <CAlert color="danger" closeButton show={invalidUser} style={{ marginTop: 10 }}>
-                      Invalid username / password
+    <div className="c-app c-default-layout flex-row align-items-center" style={homeImage}>
+      <CContainer >
+        <CRow className="justify-content-center">
+          <CCol md="7">
+            <CCardGroup>
+              <CCard className="p-4" style={{ backgroundColor: 'white', opacity: 0.9 }}>
+                <CCardBody className="text-center">
+                  <CForm>
+                    <span className="display-4">Welcome to NUSTeams</span>
+                    <p>Centralized collaboration platform for NUS students</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput color="black" type="text" placeholder="Username" autoComplete="username" onChange={handleEmailChange} />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder="Password" autoComplete="current-password" onChange={handlePasswordChange} />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6" className="text-left">
+                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                      </CCol>
+                      <CCol xs="6" className="text-right">
+                        <CButton color="primary" className="px-4" onClick={doLogin}>Login</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                  <CAlert color="danger" closeButton show={invalidUser} style={{ marginTop: 10 }}>
+                    Invalid username / password
                     </CAlert>
-                  </CCardBody>
-                </CCard>
-              </CCardGroup>
-            </CCol>
-          </CRow>
-        </CContainer>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
     </div>
   )
 }
